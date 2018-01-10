d8e183878432bf6fa22ef87987f4e05dfbaaa73d
luckyboy1102 <chenshuai3333@sina.com>
2018-01-10 21:01:30 +0800

    Impl browser

--- app/src/main/java/edu/njupt/xfilter/BrowserActivity.java
+++ app/src/main/java/edu/njupt/xfilter/BrowserActivity.java
@@ -1,8 +1,16 @@
 package edu.njupt.xfilter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.webkit.WebView;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by Chen on 2018/1/10.
@@ -10,9 +18,30 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BrowserActivity extends AppCompatActivity {
 
+    @BindView(R.id.edit_url)
+    EditText mUrl;
+    @BindView(R.id.webview)
+    WebView webView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+    }
+
+    @OnClick(R.id.button_go)
+    void safari() {
+        String url = mUrl.getText().toString();
+        if (!TextUtils.isEmpty(url)) {
+            if (!url.startsWith("http://") || !url.startsWith("https://")) {
+                url = "http://" + url;
+            }
+            webView.loadUrl(url);
+        }
+    }
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, BrowserActivity.class);
+        context.startActivity(starter);
     }
 }
--- app/src/main/java/edu/njupt/xfilter/MainActivity.java
+++ app/src/main/java/edu/njupt/xfilter/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.OnClick;
 
 /**
  * Created by Chen on 2018/1/10.
@@ -16,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @OnClick({R.id.button_browser})
+    void itemClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_browser:
+                BrowserActivity.start(this);
+                break;
+            default:
+                break;
+        }
     }
 
     public static void start(Context context) {
