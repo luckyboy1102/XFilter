aebb78f9e6d842927a8a0e5d50aff9a98e106ee3
luckyboy1102 <chenshuai3333@sina.com>
2018-01-10 17:10:27 +0800

    Project init

---
+++ app/src/main/java/edu/njupt/xfilter/MainActivity.java
@@ -0,0 +1,25 @@
+package edu.njupt.xfilter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chen on 2018/1/10.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, MainActivity.class);
+        context.startActivity(starter);
+    }
+}
