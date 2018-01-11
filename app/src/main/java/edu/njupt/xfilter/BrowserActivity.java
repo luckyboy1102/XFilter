b51a258d08f51504804f5df140f346a59f1d9661
luckyboy1102 <chenshuai3333@sina.com>
2018-01-11 16:35:02 +0800

    Impl backward

--- app/src/main/java/edu/njupt/xfilter/BrowserActivity.java
+++ app/src/main/java/edu/njupt/xfilter/BrowserActivity.java
@@ -78,6 +78,18 @@ public class BrowserActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
     public static void start(Context context) {
         Intent starter = new Intent(context, BrowserActivity.class);
         context.startActivity(starter);
